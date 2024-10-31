fc44282beb91733ec2f49e851b6499cbc944895d
Jáchym Buben <jachym.buben@seznam.cz>
2024-10-31 20:38:06 +0100

    Better RAM overflow handling

--- src/eventEnergy.py
+++ src/eventEnergy.py
@@ -9,3 +9,6 @@ def eventEnergy(events):
     # free RAM memory
     del events
     gc.collect()    # force it
+    # clear RAM cache if run on POSIX system
+    if os.name == 'posix':
+        os.system("sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'")
